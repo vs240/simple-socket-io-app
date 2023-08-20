f5ac98bdb0dfeb5d82fa3ac5980a977b78749e6f
vs240 <vaibhavshukla5388@gmail.com>
2023-08-20 17:05:11 +0530

    second commit

--- index1.js
+++ index1.js
@@ -1,15 +1,29 @@
-import express from "express";
-import superheroes from "superheroes";
+//import express from "express";
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
-const port = 3000;
+const server = http.createServer(app);
+const io = socketIo(server);
 
-
-app.get("/", (req, res) => {
-  let name = superheroes.random();
-  res.send(`<h1>Behold, I am ${name}!</h1>`);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast the message to all connected clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
